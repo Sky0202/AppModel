e561e9642d5ff85f835ff094645a8baf9a3b48cd
niuzhixun <niuzhixun@163.com>
2018-03-28 15:47:53 +0800

    框架优化

--- app/src/main/java/com/fanqie/appmodel/main/activity/MainActivity.java
+++ app/src/main/java/com/fanqie/appmodel/main/activity/MainActivity.java
@@ -42,10 +42,6 @@ public class MainActivity extends BaseActivity {
         vpContainer.setAdapter(mainPagerAdapter);
 //        vpContainer.setOffscreenPageLimit(4);  // 预加载 4 页
 
-        int mode = StatusBarUtils.StatusBarLightMode(this);
-
-//        DebugLog.e(mode + "");
-
     }
 
     @Override
